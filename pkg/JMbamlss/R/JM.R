7ee3ef1788f8d14b5145f3236e20a75721f3c2be
freezenik (Nikolaus Umlauf) <nikolaus.umlauf@uibk.ac.at>
2021-08-30 13:53:49 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@4177 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/JMbamlss/R/JM.R
+++ pkg/JMbamlss/R/JM.R
@@ -30,12 +30,42 @@ jm_bamlss <- function(...)
   rval
 }
 
-JM_transform <- function(object, subdivisions = 25, ...)
+## Smooth time transformer function.
+sm_time_transform <- function(x, data, grid, yname, timevar, take)
+{
+  if(!is.null(take))
+    data <- data[take, , drop = FALSE]
+  X <- NULL
+print(x$term)
+  for(j in x$term) {
+    if((j != yname) & (j != timevar)) {
+      df <- data.frame(rep(data[[j]], each = length(grid[[1]])))
+      names(df) <- j
+      X <- if(is.null(X)) df else cbind(X, df)
+    }
+  }
+  if(!is.null(X))
+    colnames(X) <- x$term[!(x$term %in% c(yname, timevar))]
+  X <- if(is.null(X)) data.frame(unlist(grid)) else cbind(X, unlist(grid))
+  colnames(X)[ncol(X)] <- yname
+  if(timevar != yname) {
+    X <- cbind(X, unlist(grid))
+    colnames(X)[ncol(X)] <- timevar
+  }
+  if(x$by != "NA" & x$by != yname)
+    X[[x$by]] <- rep(data[[x$by]], each = length(grid[[1]]))
+
+  x$Xgrid <- PredictMat(x, X)
+
+  x
+}
+
+## Compute all necessary matrices.
+JM_transform <- function(object, subdivisions = 10, timevar = NULL, ...)
 {
   stopifnot(requireNamespace("statmod"))
 
   gq <- statmod::gauss.quad(subdivisions, ...)
-  gq$weights <- gq$weights * exp(gq$nodes^2)
 
   ## Get idvar.
   class_mu <- sapply(object$x$mu$smooth.construct, class)
@@ -47,18 +77,23 @@ JM_transform <- function(object, subdivisions = 25, ...)
   idvar <- smj$term[1]
 
   ## Setup integration.
-  grid <- function(upper, length){
-    seq(from = 0, to = upper, length = length)
+  grid <- function(time) {
+    time / 2 * gq$nodes + time / 2
   }
   y2 <- cbind(object$y[[1]][, "time"], object$model.frame[[idvar]])
   colnames(y2) <- c("time", idvar)
   take <- !duplicated(y2)
   take_last <- !duplicated(y2, fromLast = TRUE)
+
   y2 <- y2[take, , drop = FALSE]
   nobs <- nrow(y2)
-  grid <- lapply(y2[, "time"], grid, length = subdivisions)
+  grid <- lapply(y2[, "time"], grid)
 
   yname <- all.names(object$x$lambda$formula[2])[2]
+  timevar_mu <- timevar
+  if(is.null(timevar_mu))
+    stop("the time variable is not specified, needed for mu!")
+  timevar <- yname
 
   ## Transform lambda.
   ## FIXME: remove intercept!
@@ -66,17 +101,35 @@ JM_transform <- function(object, subdivisions = 25, ...)
   if(!is.null(object$x$lambda$smooth.construct)) {
     for(j in names(object$x$lambda$smooth.construct)) {
       if(j != "model.matrix") {
-        xterm <- x$lambda$smooth.construct[[j]]$term
-        by <- if(x$lambda$smooth.construct[[j]]$by != "NA") {
-          x$lambda$smooth.construct[[j]]$by
+        xterm <- object$x$lambda$smooth.construct[[j]]$term
+        by <- if(object$x$lambda$smooth.construct[[j]]$by != "NA") {
+          object$x$lambda$smooth.construct[[j]]$by
         } else NULL
         object$x$lambda$smooth.construct[[j]] <- sm_time_transform(object$x$lambda$smooth.construct[[j]],
-          data[, unique(c(xterm, yname, by, timevar, idvar)), drop = FALSE],
+          object$model.frame[, unique(c(xterm, yname, by, timevar, idvar)), drop = FALSE],
           grid, yname, timevar, take)
       }
     }
   }
-print(grid)
+
+  ## FIXME: gamma + linear.
+
+  ## Setup longitudinal part.
+  if(!is.null(object$x$mu$smooth.construct)) {
+    for(j in names(object$x$mu$smooth.construct)) {
+      if(j != "model.matrix") {
+        xterm <- object$x$mu$smooth.construct[[j]]$term
+        by <- if(object$x$mu$smooth.construct[[j]]$by != "NA") {
+          object$x$mu$smooth.construct[[j]]$by
+        } else NULL
+        object$x$mu$smooth.construct[[j]] <- sm_time_transform(object$x$mu$smooth.construct[[j]],
+          object$model.frame[, unique(c(xterm, yname, by, timevar, idvar)), drop = FALSE],
+          grid, yname, timevar_mu, take_last)
+      }
+    }
+  }
+
+cat("ok\n")
 
 stop()
 }
@@ -86,12 +139,13 @@ Surv2 <- bamlss:::Surv2
 f <- list(
   Surv2(survtime, event, obs = y) ~ -1 + s(survtime),
   gamma ~ 1,
-  mu ~ -1 + marker + s(obstime, by = marker) + s(id, wfpc.1, wfpc.2, bs = "pcre"),
+  mu ~ -1 + marker + s(obstime, by = marker) +
+    s(id, wfpc.1, wfpc.2, bs = "pcre", xt = list("fpca" = 1)),
   sigma ~ 1,
   alpha ~ 1
 )
 
-b <- bamlss(f, family = jm_bamlss, data = d)
+b <- bamlss(f, family = jm_bamlss, data = d, timevar = "obstime")
 
 
 
