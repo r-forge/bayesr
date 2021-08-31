3c12e481aff9be516dd1f6083caf5c5771c888f8
alexvolkmann (Alexander Volkmann) <alexander.volkmann@hu-berlin.de>
2021-08-31 12:23:19 +0000

    restructure the evaluation functions, now it's evaluations over all markers
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@4183 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/JMbamlss/R/eval_mfun.R
+++ pkg/JMbamlss/R/eval_mfun.R
@@ -9,62 +9,55 @@
 # uFPCA <- MFPCA(sim$simData, M = 5, uniExpansions = list(list(type = "uFPCA"),
 #                                                         list(type = "uFPCA")))
 
-
-#' Evaluate 
-#' @param MFPCA
-#' @param timepoints
-eval_mfpc <- function (MFPCA, timepoints) {
-  
-  # Which observations have to be interpolated
-  # Keep track of the order to order the X values appropriately
-  grid <- unlist(argvals(MFPCA$functions), recursive = FALSE)
-  arg_vals <- mapply(function(tpoints, gpoints){
-    unique(c(gpoints, tpoints))
-  }, tpoints = timepoints, gpoints = grid, SIMPLIFY = FALSE)
-  arg_order <- lapply(arg_vals, order)
-  
-  # Attach NAs to the observed values to later interpolate them
-  obs_x <- lapply(MFPCA$functions, X)
-  obs_x <- mapply(function(dim_x, dim_a){
-    add_na <- cbind(dim_x, matrix(NA, nrow = nrow(dim_x),
-                                  ncol = length(dim_a) - ncol(dim_x)))
-    add_na
-  }, dim_x = obs_x, dim_a = arg_vals, SIMPLIFY = FALSE)
-  
-  # Reorder the argvals and xvalues
-  arg_vals <- mapply(function(tpoints, torder){
-    tpoints[torder]
-  }, tpoints = arg_vals, torder = arg_order, SIMPLIFY = FALSE)
-  obs_x <- mapply(function(xpoints, xorder){
-    xpoints[, xorder]
-  }, xpoints = obs_x, xorder = arg_order, SIMPLIFY = FALSE)
-  
-  # linear interpolation
-  na_evals <- mapply(function(xpoints, xvals) {
-    zoo::na.approx(t(xpoints), x = xvals, na.rm = FALSE)
-  }, xpoints = obs_x, xvals = arg_vals, SIMPLIFY = FALSE)
-
-  # extract
-  mapply(function (dim, tpoint) {
-    rownames(dim) <- tpoint
-    dim[paste(timepoints), ]
-  }, dim = na_evals, tpoint = arg_vals, SIMPLIFY = FALSE)
-}
-
-
-# fu <- funData(argvals = seq(0, 1, length = 10), 
-#               X = matrix(sin(seq(0, 3, length = 10)), nrow = 1))
-# eval_fundata(fu, 0.39)
+# fu_onen <- funData(argvals = seq(0, 1, length = 10),
+#                    X = matrix(sin(seq(0, 3, length = 10)), nrow = 1))
+# fu_muln <- funData(argvals = seq(0, 1, length = 10),
+#                    X = matrix(c(sin(seq(0, 3, length = 10)),
+#                                 cos(seq(0, 3, length = 10))), 
+#                               ncol = 10, nrow = 2, byrow = TRUE))
 
 eval_fundata <- function(funData, evalpoints) {
+  # Extract info
   args <- unlist(argvals(funData))
   arglength <- length(args)
   evalength <- length(evalpoints)
   indx <- seq_len(arglength + evalength)
   argfull <- c(args, evalpoints)
   order <- order(argfull)
-  xobs <- cbind(X(funData),
-                matrix(NA, ncol = evalength, nrow = nrow(X(funData))))[, order]
-  eval <- zoo::na.approx(t(xobs), x = argfull[order], na.rm = FALSE)
-  eval[match(arglength + seq_len(evalength), order), ]
+  n <- nrow(X(funData))
+  # Blow up matrix with NA values and order it
+  na_mat <- matrix(NA, ncol = evalength, nrow = n)
+  xobs <- cbind(X(funData), na_mat)[, order]
+  # Different cases as cbind returns vector when n == 1
+  if (n > 1) {
+    eval <- zoo::na.approx(t(xobs), x = argfull[order], na.rm = FALSE)
+    matrix(eval[match(arglength + seq_len(evalength), order), ], ncol = n,
+           nrow = evalength)
+  } else {
+    eval <- zoo::na.approx(xobs, x = argfull[order], na.rm = FALSE)
+    matrix(eval[match(arglength + seq_len(evalength), order)], ncol = n, 
+           nrow = evalength)
+  }
 }
+
+# eval_fundata(fu_onen, 0.39)
+# eval_fundata(fu_muln, 0.39)
+# eval_fundata(fu_onen, c(0.39, 0.7))
+# eval_fundata(fu_muln, c(0.39, 0.7))
+
+
+eval_mfpc <- function(mfpca, timepoints) {
+  eigenfct <- mfpca$functions
+  eigenval <- mfpca$values
+  K <- length(eigenfct)
+  # predict the same time points for all markers
+  prm <- do.call(rbind, lapply(eigenfct, eval_fundata, evalpoints = timepoints))
+  colnames(prm) <- paste0("wfpc.", seq_len(ncol(prm)))
+  rownames(prm) <- paste0("m", rep(seq_len(K), each = length(timepoints)))
+  # weight with eigenvalues
+  t(t(prm) * sqrt(eigenval))
+}
+
+# eval_mfpc(uFPCA, 0.39)
+# eval_mfpc(uFPCA, c(0.39, 0.41))
+# eval_mfpc(uFPCA, c(0.39, 0.7))
