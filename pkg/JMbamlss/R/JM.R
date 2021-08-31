4e7e0e2941be6ed4141a9d2f0f3ee29ddd2c1e66
freezenik (Nikolaus Umlauf) <nikolaus.umlauf@uibk.ac.at>
2021-08-31 14:34:39 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@4184 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/JMbamlss/R/JM.R
+++ pkg/JMbamlss/R/JM.R
@@ -6,7 +6,7 @@ source("simMultiJM.R")
 source("eval_mfun.R")
 
 if(FALSE) {
-  d <- simMultiJM()
+  d <- simMultiJM(nsub = 50)
 
   marker_dat <- split(d, d$marker)
   marker_dat <- lapply(marker_dat, function (mark) {
@@ -25,11 +25,11 @@ if(FALSE) {
   uniExpansions <- lapply(FPCA, function (mark) {
     list(type = "given", functions = mark$functions)
   })
-  MFPCA <- MFPCA(mFData = mFData, M = 3, uniExpansions = uniExpansions)
+  MFPCA <- MFPCA(mFData = mFData, M = 2, uniExpansions = uniExpansions)
  
 }
 
-jm_bamlss <- function(...)
+mjm_bamlss <- function(...)
 {
   links = c(
     lambda = "log",
@@ -40,11 +40,11 @@ jm_bamlss <- function(...)
   )
   
   rval <- list(
-    "family" = "jm",
+    "family" = "mjm",
     "names" = c("lambda", "gamma", "mu", "sigma", "alpha"),
     "links" = links,
-    "transform" = JM_transform,
-    "optimizer" = NULL,
+    "transform" = MJM_transform,
+    "optimizer" = opt_MJM,
     "sampler" = NULL,
     "predict" = NULL
   )
@@ -54,7 +54,7 @@ jm_bamlss <- function(...)
 }
 
 ## Smooth time transformer function.
-sm_time_transform <- function(x, data, grid, yname, timevar, take)
+sm_time_transform_mjm <- function(x, data, grid, yname, timevar, take)
 {
   if(!is.null(take))
     data <- data[take, , drop = FALSE]
@@ -83,7 +83,7 @@ sm_time_transform <- function(x, data, grid, yname, timevar, take)
 }
 
 ## PCRE transformer.
-sm_time_transform_pcre <- function(x, data, grid, yname, timevar, take)
+sm_time_transform_mjm_pcre <- function(x, data, grid, yname, timevar, take, N)
 {
   if(!is.null(take))
     data <- data[take, , drop = FALSE]
@@ -108,6 +108,7 @@ sm_time_transform_pcre <- function(x, data, grid, yname, timevar, take)
 
   class(x) <- "pcre2.random.effect"
 
+  x$N <- N
   x$term <- c(x$term, timevar)
   x$timevar <- timevar
   x$Xgrid <- PredictMat(x, X)
@@ -119,14 +120,45 @@ Predict.matrix.pcre2.random.effect <- function(object, data)
 {
   if(is.null(object$xt$mfpc))
     stop("need mfpa object!")
-print(head(as.data.frame(data)))
-  w <- eval_mfpc(object$xt$mfpc, data[[object$timevar]])
-print(w)
-stop()
+  X <- eval_mfpc(object$xt$mfpc, data[[object$timevar]][1:object$N])
+  if(ncol(X) != (length(object$term) - 2))
+    stop("check M argument in MFPCA()!")
+  X <- cbind(data[[object$term[1]]], X)
+  colnames(X) <- object$term[-length(object$term)]
+  return(X)
 }
 
+## Linear design transformer.
+param_time_transform_mjm <- function(x, formula, data, grid, yname, timevar, take)
+{
+  if(!is.null(take))
+    data <- data[take, , drop = FALSE]
+  X <- Xn <- NULL
+  for(j in names(data)) {
+    if((!grepl("Surv(", j, fixed = TRUE) & !grepl("Surv2(", j, fixed = TRUE)) & (j != yname) & (j != timevar)) {
+      df <- data.frame(rep(data[[j]], each = length(grid[[1]])))
+      names(df) <- j
+      X <- if(is.null(X)) df else cbind(X, df)
+      Xn <- c(Xn, j)
+    }
+  }
+  if(!is.null(X))
+    colnames(X) <- Xn
+  X <- if(is.null(X)) data.frame(unlist(grid)) else cbind(X, unlist(grid))
+  colnames(X)[ncol(X)] <- yname
+  if(timevar != yname) {
+    X <- cbind(X, unlist(grid))
+    colnames(X)[ncol(X)] <- timevar
+  }
+
+  x$Xgrid <- model.matrix(formula, data = X)
+
+  x
+}
+
+
 ## Compute all necessary matrices.
-JM_transform <- function(object, subdivisions = 10, timevar = NULL, ...)
+MJM_transform <- function(object, subdivisions = 10, timevar = NULL, ...)
 {
   stopifnot(requireNamespace("statmod"))
 
@@ -150,6 +182,7 @@ JM_transform <- function(object, subdivisions = 10, timevar = NULL, ...)
 
   take_l <- !duplicated(y2[, 1:2])
   take_last_l <- !duplicated(y2, fromLast = TRUE)
+  nsubj <- length(unique(y2[, idvar]))
 
   marker <- FALSE
   if(!is.null(object$model.frame$marker)) {
@@ -170,7 +203,26 @@ JM_transform <- function(object, subdivisions = 10, timevar = NULL, ...)
     stop("the time variable is not specified, needed for mu!")
   timevar <- yname
 
-  ## FIXME: remove intercepts!
+  ## The basic setup.
+  if(is.null(attr(object$x, "bamlss.engine.setup")))
+    object$x <- bamlss.engine.setup(object$x, ...)
+
+  ## Remove intercept from lambda.
+  if(!is.null(object$x$lambda$smooth.construct$model.matrix)) {
+    cn <- colnames(object$x$lambda$smooth.construct$model.matrix$X)
+    if("(Intercept)" %in% cn)
+      object$x$lambda$smooth.construct$model.matrix$X <- object$x$lambda$smooth.construct$model.matrix$X[, cn != "(Intercept)", drop = FALSE]
+    if(ncol(x$lambda$smooth.construct$model.matrix$X) < 1) {
+      object$x$lambda$smooth.construct$model.matrix <- NULL
+      object$x$lambda$terms <- drop.terms.bamlss(x$lambda$terms, pterms = FALSE, keep.intercept = FALSE)
+    } else {
+      object$x$lambda$smooth.construct$model.matrix$term <- gsub("(Intercept)+", "",
+        object$x$lambda$smooth.construct$model.matrix$term, fixed = TRUE)
+      object$x$lambda$smooth.construct$model.matrix$state$parameters <- object$x$lambda$smooth.construct$model.matrix$state$parameters[-1]
+      object$x$lambda$terms <- drop.terms.bamlss(x$lambda$terms,
+        pterms = TRUE, sterms = TRUE, keep.intercept = FALSE)
+    }
+  }
 
   ## Compute new design matrices for integration for smooth terms.
   for(i in c("lambda", "mu", "alpha")) {
@@ -182,11 +234,12 @@ JM_transform <- function(object, subdivisions = 10, timevar = NULL, ...)
             object$x[[i]]$smooth.construct[[j]]$by
           } else NULL
           if(inherits(object$x[[i]]$smooth.construct[[j]], "pcre.random.effect")) {
-            object$x[[i]]$smooth.construct[[j]] <- sm_time_transform_pcre(object$x[[i]]$smooth.construct[[j]],
+            object$x[[i]]$smooth.construct[[j]] <- sm_time_transform_mjm_pcre(object$x[[i]]$smooth.construct[[j]],
               object$model.frame[, unique(c(xterm, yname, by, timevar_mu, idvar)), drop = FALSE],
-              grid, yname, timevar_mu, if(i == "lambda") take_last_l else take_last)
+              grid, yname, timevar_mu, if(i == "lambda") take_last_l else take_last,
+              N = nsubj * subdivisions)
           } else {
-            object$x[[i]]$smooth.construct[[j]] <- sm_time_transform(object$x[[i]]$smooth.construct[[j]],
+            object$x[[i]]$smooth.construct[[j]] <- sm_time_transform_mjm(object$x[[i]]$smooth.construct[[j]],
               object$model.frame[, unique(c(xterm, yname, by, if(i == "mu") timevar_mu else timevar, idvar)), drop = FALSE],
               grid, yname, if(i == "mu") timevar_mu else timevar, if(i == "lambda") take_last_l else take_last)
           }
@@ -195,9 +248,64 @@ JM_transform <- function(object, subdivisions = 10, timevar = NULL, ...)
     }
   }
 
-cat("ok\n")
+  ## Now linear part.
+  for(i in c("lambda", "mu", "alpha")) {
+    if(!is.null(object$x[[i]]$smooth.construct$model.matrix)) {
+      object$x[[i]]$smooth.construct$model.matrix <- param_time_transform_mjm(object$x[[i]]$smooth.construct$model.matrix,
+        bamlss:::drop.terms.bamlss(object$x[[i]]$terms, sterms = FALSE, keep.response = FALSE), object$model.frame, grid, yname, 
+        if(i != "mu") timevar else timevar_mu, if(i == "lambda") take_last_l else take_last)
+    }
+  }
 
-stop()
+  ## Update prior/grad/hess functions
+  for(j in names(object$x)) {
+    for(sj in names(object$x[[j]]$smooth.construct)) {
+      priors <- bamlss:::make.prior(object$x[[j]]$smooth.construct[[sj]])
+      object$x[[j]]$smooth.construct[[sj]]$prior <- priors$prior
+      object$x[[j]]$smooth.construct[[sj]]$grad <- priors$grad
+      object$x[[j]]$smooth.construct[[sj]]$hess <- priors$hess
+    }
+  }
+
+  return(object)
+}
+
+
+opt_MJM <- function(x, y, eps = 0.0001, maxit = 400, nu = 0.1, ...)
+{
+  y <- y[[1]]
+
+  eta <- bamlss:::get.eta(x, expand = FALSE)
+
+  eps0 <- eps + 1
+  eta0 <- do.call("cbind", eta)
+  iter <- 0
+
+  while((eps0 > eps) & (iter < maxit)) {
+    ## (1) update lambda.
+    for(j in names(x$lambda$smooth.construct)) {
+      state <- update_mjm_lambda(x$lambda$smooth.construct[[j]], nu = nu, ...)
+    }
+  }
+}
+
+update_mjm_lambda <- function(x, nu, ...)
+{
+  ## grid matrix -> x$Xgrid
+  ## design matrix -> x$X
+  ## penalty matrices -> x$S
+  ## optimizer.R -> bfit_iwls() updating.
+  b <- bamlss::get.state(x, "b")
+  tau2 <- bamlss::get.state(x, "tau2")
+
+print(x$state)
+print(x$term)
+print(x$label)
+print(tau2)
+stop("!\n")
+
+  ## Newton-Raphson.
+  ## b <- b + nu * H %*% grad
 }
 
 Surv2 <- bamlss:::Surv2
@@ -211,7 +319,7 @@ f <- list(
   alpha ~ -1 + marker + s(survtime, by = marker)
 )
 
-b <- bamlss(f, family = jm_bamlss, data = d, timevar = "obstime")
+b <- bamlss(f, family = mjm_bamlss, data = d, timevar = "obstime")
 
 
 
