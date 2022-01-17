49002a2786b21b09bc0e5c296fb38d5afb478599
alexvolkmann (Alexander Volkmann) <alexander.volkmann@hu-berlin.de>
2022-01-17 14:05:25 +0000

    Include examples to run bamlss.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@4266 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/JMbamlss/inst/bamlss_examples.R
@@ -0,0 +1,39 @@
+library(bamlss)
+data("pbc2", package = "JMbayes")
+
+## Set up the model formula including
+## functional random intercepts using ti().
+f <- list(
+  Surv2(years, status2, obs = log(serBilir)) ~ s(years,k=20),
+  gamma ~ s(age,k=20) + drug + sex,
+  mu ~ ti(id,bs="re") + 
+    ti(year,k=20) + 
+    ti(id,year,bs=c("re","cr"),k=c(nlevels(pbc2$id),8)),
+  sigma ~ 1,
+  alpha ~ s(years,k=20),
+  dalpha ~ -1
+)
+## Set the seed for reproducibility.
+set.seed(123)
+
+debug(bamlss:::sam_JM)
+## Estimate model.
+b <- bamlss(f, data = pbc2, family = "jm",
+            timevar = "year", idvar = "id")
+
+
+# --- Code from JM.R
+# 312 Individuen und 25 Integrationspunkte = 7800 Zeilen
+# für s(years) term, k = 20 -> also 19 Parameter = 19 Spalten
+X <- x$fit.fun_timegrid(NULL)
+
+# eta_timegrid: für jeden Integrationspunkt sind die zeitvariierenden etas 
+#   ausgewertet und aufaddiert
+eeta <- exp(eta_timegrid)
+
+# Integral wird berechnet? Funktion in C
+# 
+int <- survint(X, eeta, width, exp(eta$gamma))
+# returns negative Hessian
+
+xgrad <- drop(t(status) %*% x$XT - int$grad)
