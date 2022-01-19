d6a7dec0d1689eb7179cea6fea70020020a17c5c
alexvolkmann (Alexander Volkmann) <alexander.volkmann@hu-berlin.de>
2022-01-19 16:08:38 +0000

    Finish first version for lambda proposals and restructure.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@4271 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/JMbamlss/R/mcmc_proposing.R
+++ pkg/JMbamlss/R/mcmc_proposing.R
@@ -1,10 +1,187 @@
 
+# Proposals for all predictors --------------------------------------------
+
+propose_mjm <- function(predictor, x, y, eta, eta_timegrid, eta_T, survtime,
+                        eta_timegrid_alpha, eta_timegid_mu, eta_timegrid_long,
+                        eta_timegrid_lambda, logLik_old, nsubj, gq_weights,
+                        status, nu) {
+  
+  # Get old parameters
+  b_old <- bamlss::get.state(x, "b")
+  p_old <- x$prior(x$state$parameters)
+  
+  
+  # Calculate Newton step based on old parameter
+  switch(predictor,
+    "lambda" = {
+      int_i <- survint_gq(pred = "lambda", pre_fac = exp(eta$gamma),
+                          omega = exp(eta_timegrid),
+                          int_vec = x$Xgrid, weights = attr(y, "gq_weights"),
+                          survtime = survtime)
+      x_score <- drop(attr(y, "status") %*% x$XT) - colSums(int_i$score_int)
+      x_H <- matrix(colSums(int_i$hess_int), ncol = length(b_old))
+    },
+    "gamma" = {
+      
+    },
+    "alpha" = {
+      
+    },
+    "mu" = {
+      
+    },
+    "sigma" = {
+      
+    })
+  
+  # Include priori in derivatives
+  x_score <- x_score + x$grad(score = NULL, x$state$parameters, full = FALSE)
+  x_H <- x_H + x$hess(score = NULL, x$state$parameters, full = FALSE)
+  
+  # Compute the inverse of the hessian.
+  Sigma <- bamlss:::matrix_inv(x_H, index = NULL)
+  
+  # Get new location parameter for proposal
+  mu_prop <- drop(b_old + nu * Sigma %*% x_score )
+  
+  
+  # Sample new parameters.
+  b_prop <- drop(mvtnorm::rmvnorm(n = 1, mean = mu_prop, sigma = Sigma,
+                                  method="chol"))
+  names(b_prop) <- names(b_old)
+  x$state$parameters <- bamlss::set.par(x$state$parameters, b_prop, "b")
+  p_prop <- x$prior(x$state$parameters)
+  
+  # Compute proposal density of proposed parameter given old
+  q_prop_giv_old <- mvtnorm::dmvnorm(b_prop, mean = mu_prop, sigma = Sigma, 
+                                     log = TRUE)
+  
+  
+  # Update additive predictors and x$state
+  switch(predictor,
+    "lambda" = {
+      
+      # timegrids
+      fitted_timegrid_prop <- drop(x$Xgrid %*% b_prop)
+      eta_timegrid_lambda <- eta_timegrid_lambda - x$state$fitted_timegrid + 
+        fitted_timegrid_prop
+      eta_timegrid <- eta_timegrid_lambda + eta_timegrid_long
+      
+      # fitted values
+      fit_prop <- drop(x$X %*% b_prop)
+      eta_T <- eta_T - eta$lambda
+      eta$lambda <- eta$lambda - fitted(x$state) + fit_prop
+      eta_T <- eta_T + eta$lambda
+      
+      # state
+      x$state$fitted_timegrid <- fitted_timegrid_prop
+      x$state$fitted.values <- fit_prop
+      
+    },
+   "gamma" = {
+     
+   },
+   "alpha" = {
+     
+   },
+   "mu" = {
+     
+   },
+   "sigma" = {
+     
+   })
+
+  
+  # New logLik
+  sum_Lambda <- (survtime/2 * exp(eta$gamma)) %*%
+    (diag(nsubj)%x%t(gq_weights))%*%
+    exp(eta_timegrid)
+  logLik <- drop(status %*% eta_T - sum_Lambda) +
+    sum(dnorm(y[[1]][, "obs"], mean = eta$mu, sd = exp(eta$sigma),
+              log = TRUE))
+  
+  # Calculate Newton step based on proposed parameter and updated etas
+  switch(predictor,
+    "lambda" = {
+      int_i <- survint_gq(pred = "lambda", pre_fac = exp(eta$gamma),
+                          omega = exp(eta_timegrid),
+                          int_vec = x$Xgrid, weights = attr(y, "gq_weights"),
+                          survtime = survtime)
+      x_score <- drop(attr(y, "status") %*% x$XT) - colSums(int_i$score_int)
+      x_H <- matrix(colSums(int_i$hess_int), ncol = length(b_old))
+    },
+         "gamma" = {
+           
+         },
+         "alpha" = {
+           
+         },
+         "mu" = {
+           
+         },
+         "sigma" = {
+           
+         })
+  x_score <- x_score + x$grad(score = NULL, x$state$parameters, full = FALSE)
+  x_H <- x_H + x$hess(score = NULL, x$state$parameters, full = FALSE)
+  Sigma <- bamlss:::matrix_inv(x_H, index = NULL)
+  
+  # Get new location parameter for proposal
+  mu <- drop(b_prop + nu * Sigma %*% x_score)
+  
+  # Proposal density of old parameter given proposed
+  q_old_giv_prop <- mvtnorm::dmvnorm(b_old, mean = mu, sigma = Sigma, 
+                                     log = TRUE)
+  
+  
+  ## Compute acceptance probablity.
+  x$state$alpha <- drop((logLik + q_old_giv_prop + p_prop) - 
+                          (logLik_old + q_prop_giv_old + p_old))
+  
+  
+  # WIE KOMMTS?
+  ## Save edf.
+  x$state$edf <- bamlss:::sum_diag(
+    matrix(colSums(int_i$hess_int), ncol = length(b_prop)) %*% Sigma)
+  
+  
+  ## Sample variance parameter.
+  if(!x$fixed & is.null(x$sp) & length(x$S)) {
+    if((length(x$S) < 2) & (attr(x$prior, "var_prior") == "ig")) {
+      g <- bamlss::get.par(x$state$parameters, "b")
+      a <- x$rank / 2 + x$a
+      b <- 0.5 * crossprod(g, x$S[[1]]) %*% g + x$b
+      tau2 <- 1 / rgamma(1, a, b)
+      x$state$parameters <- bamlss::set.par(x$state$parameters, tau2, "tau2")
+    } else {
+      i <- grep("tau2", names(x$state$parameters))
+      for(j in i) {
+        x$state$parameters <- bamlss:::uni.slice(
+          x$state$parameters, x, NULL, NULL, NULL, id = "mu", j, 
+          logPost = uni.slice_tau2_logPost, lower = 0, ll = 0)
+      }
+    }
+  }
+  
+
+  
+  return(list(xstate = x$state,
+              etas = switch(predictor,
+                            "lambda" = list(eta = eta, eta_T = eta_T, 
+                                            eta_timegrid = eta_timegrid, 
+                                            eta_timegrid_lambda = 
+                                              eta_timegrid_lambda)),
+              logLik = logLik))
+  
+}
+
+
+
 # Proposals for lambda ----------------------------------------------------
 
 propose_mjm_lambda <- function(x, y, eta, eta_timegrid, eta_timegrid_lambda, 
-                               eta_T, survtime, nu, ...){
+                               eta_T, survtime, nu, eta_timegrid_long, ...){
   
-  browser()
   b_old <- bamlss::get.state(x, "b")
   
   # Old logLikelihood and prior.
@@ -13,7 +190,7 @@ propose_mjm_lambda <- function(x, y, eta, eta_timegrid, eta_timegrid_lambda,
   status <- attr(y, "status")
 
   sum_Lambda <- (survtime/2 * exp(eta$gamma)) %*%
-    (diag(nsubj)%x%t(gq_weights))%*%
+    (diag(nsubj)%x%t(gq_weights))%*% 
     exp(eta_timegrid)
   logLik_old <- drop(status %*% eta_T - sum_Lambda) +
     sum(dnorm(y[[1]][, "obs"], mean = eta$mu, sd = exp(eta$sigma),
@@ -26,7 +203,7 @@ propose_mjm_lambda <- function(x, y, eta, eta_timegrid, eta_timegrid_lambda,
                       int_vec = x$Xgrid, weights = attr(y, "gq_weights"),
                       survtime = survtime)
   x_score <- drop(attr(y, "status") %*% x$XT) - colSums(int_i$score_int)
-  x_H <- matrix(colSums(int_i$hess_int), ncol = b_p)
+  x_H <- matrix(colSums(int_i$hess_int), ncol = length(b_old))
   
   x_score <- x_score + x$grad(score = NULL, x$state$parameters, full = FALSE)
   x_H <- x_H + x$hess(score = NULL, x$state$parameters, full = FALSE)
@@ -38,22 +215,24 @@ propose_mjm_lambda <- function(x, y, eta, eta_timegrid, eta_timegrid_lambda,
   mu_prop <- drop(b_old + nu * Sigma %*% x_score )
   
   # Sample new parameters.
-  b_prop <- drop(rmvnorm(n = 1, mean = mu_prop, sigma = Sigma, method="chol"))
+  b_prop <- drop(mvtnorm::rmvnorm(n = 1, mean = mu_prop, sigma = Sigma,
+                                  method="chol"))
   names(b_prop) <- names(b_old)
   x$state$parameters <- bamlss::set.par(x$state$parameters, b_prop, "b")
   
   # Compute log priors.
   p_prop <- x$prior(x$state$parameters)
-  qbeta_prop <- dmvnorm(b_prop, mean = mu_prop, sigma = Sigma, log = TRUE)
+  q_prop_giv_old <- mvtnorm::dmvnorm(b_prop, mean = mu_prop, sigma = Sigma, 
+                                 log = TRUE)
   
   # Update additive predictors
-  fitted_timegrid_prop <- drop(x$Xgrid %*% b)
+  fitted_timegrid_prop <- drop(x$Xgrid %*% b_prop)
   eta_timegrid_lambda_prop <- eta_timegrid_lambda - x$state$fitted_timegrid + 
     fitted_timegrid_prop
   x$state$fitted_timegrid <- fitted_timegrid_prop
   eta_timegrid_prop <- eta_timegrid_lambda + eta_timegrid_long
   
-  fit_prop <- drop(x$X %*% g)
+  fit_prop <- drop(x$X %*% b_prop)
   eta_prop <- eta
   eta_prop$lambda <- eta_prop$lambda - fitted(x$state) + fit_prop
   x$state$fitted.values <- fit_prop
@@ -76,46 +255,48 @@ propose_mjm_lambda <- function(x, y, eta, eta_timegrid, eta_timegrid_lambda,
                            survtime = survtime)
   x_score_prop <- drop(attr(y, "status") %*% x$XT) - 
     colSums(int_i_prop$score_int)
-  x_H_prop <- matrix(colSums(int_i_prop$hess_int), ncol = b_prop)
+  x_H_prop <- matrix(colSums(int_i_prop$hess_int), ncol = length(b_prop))
   
   x_score_prop <- x_score_prop + x$grad(score = NULL, x$state$parameters,
                                         full = FALSE)
   x_H_prop <- x_H_prop + x$hess(score = NULL, x$state$parameters, full = FALSE)
   
   # Compute the inverse of the hessian.
-  Sigma_prop <- bamlss:::matrix_inv(x_H, index = NULL)
+  Sigma_prop <- bamlss:::matrix_inv(x_H_prop, index = NULL)
   
   # Get new location parameter for proposal
   mu_prop <- drop(b_prop + nu * Sigma_prop %*% x_score_prop)
   
   
   # Prior prob
-  qbeta <- dmvnorm(b_old, mean = mu_prop, sigma = Sigma_prop, log = TRUE)
-  
-  
+  q_old_giv_prop <- mvtnorm::dmvnorm(b_old, mean = mu_prop, sigma = Sigma_prop, 
+                                log = TRUE)
   
   ## Save edf.
-  x$state$edf <- sum_diag(int$hess %*% Sigma2)
+  x$state$edf <- bamlss:::sum_diag(
+    matrix(colSums(int_i_prop$hess_int), ncol = length(b_prop)) %*% Sigma_prop)
   
   ## Sample variance parameter.
   if(!x$fixed & is.null(x$sp) & length(x$S)) {
     if((length(x$S) < 2) & (attr(x$prior, "var_prior") == "ig")) {
-      g <- get.par(x$state$parameters, "b")
+      g <- bamlss::get.par(x$state$parameters, "b")
       a <- x$rank / 2 + x$a
       b <- 0.5 * crossprod(g, x$S[[1]]) %*% g + x$b
       tau2 <- 1 / rgamma(1, a, b)
-      x$state$parameters <- set.par(x$state$parameters, tau2, "tau2")
+      x$state$parameters <- bamlss::set.par(x$state$parameters, tau2, "tau2")
     } else {
       i <- grep("tau2", names(x$state$parameters))
       for(j in i) {
-        x$state$parameters <- uni.slice(x$state$parameters, x, NULL, NULL,
-                                        NULL, id = "mu", j, logPost = uni.slice_tau2_logPost, lower = 0, ll = 0)
+        x$state$parameters <- bamlss:::uni.slice(
+          x$state$parameters, x, NULL, NULL, NULL, id = "mu", j, 
+          logPost = uni.slice_tau2_logPost, lower = 0, ll = 0)
       }
     }
   }
   
   ## Compute acceptance probablity.
-  x$state$alpha <- drop((pibetaprop + qbeta + p2) - (pibeta + qbetaprop + p1))
-  # cat(paste("\n",pibeta, pibetaprop, qbeta, qbetaprop, p1, p2, x$state$alpha, sep=";"))
+  x$state$alpha <- drop((logLik_prop + q_old_giv_prop + p_prop) - 
+                          (logLik_old + q_prop_giv_old + p_old))
+  
   return(x$state)
 }
