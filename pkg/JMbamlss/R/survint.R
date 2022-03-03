b34f095df9b340ad5bd9cecd8361d26cadb09185
freezenik (Nikolaus Umlauf) <nikolaus.umlauf@uibk.ac.at>
2022-03-03 13:54:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@4300 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/JMbamlss/R/survint.R
+++ pkg/JMbamlss/R/survint.R
@@ -1,127 +1,153 @@
 # # New version of survint_gq with FOR loops --------------------------------
 # 
-# survint_gq <- function (pred = c("lambda", "gamma", "long"), pre_fac,
-#                         pre_vec = NULL, omega, int_fac = NULL,
-#                         int_vec = NULL, weights, survtime) {
-#   
-#   # Helpful quantities
-#   nw <- length(weights)
-#   
-#   # Set up the matrices for score and hess
-#   nsubj <- length(survtime)
-#   p <- if (pred == "gamma") ncol(pre_vec) else ncol(int_vec)
-#   score_int <- matrix(0, nrow = nsubj, ncol = p)
-#   hess_int <- matrix(0, nrow = nsubj, ncol = p^2)
-#   
-#   switch(pred, 
-#          "lambda" = {
-#            
-#            # Iterate over subjects
-#            for (ni in seq_len(nsubj)) {
-#              
-#              score_i <- 0
-#              hess_i <- 0
-#              
-#              # Iterate over GQ evaluation points
-#              for (wi in seq_along(weights)) {
-#                
-#                tmp <- weights[wi]*omega[(ni-1)*nw + wi]
-#                hess <- vector(length = p^2)
-#                # Create crossproduct for each row
-#                for (pi in seq_len(p)) {
-#                  for (pi_cross in seq_len(pi)){
-#                    hess[(pi-1)*p+pi_cross] <- hess[(pi_cross-1)*p+pi] <- 
-#                      int_vec[(ni-1)*nw + wi, pi] *
-#                      int_vec[(ni-1)*nw + wi, pi_cross]
-#                  }
-#                }
-#                score_i <- score_i + tmp*int_vec[(ni-1)*nw + wi, ]
-#                hess_i <- hess_i + tmp*hess
-#                
-#              }
-#              
-#              # Individual weights
-#              tmp <- survtime[ni] / 2 * pre_fac[ni]
-#              score_int[ni,] <- tmp * score_i
-#              hess_int[ni,] <- tmp * hess_i
-#              
-#            }
-#          },
-#          "gamma" = {
-#            
-#            # Iterate over individuals
-#            for (ni in seq_len(nsubj)) {
-#              
-#              # Integral is a scalar value
-#              survint_i <- 0
-#              for (wi in seq_len(nw)) {
-#                survint_i <- survint_i +
-#                  omega[(ni - 1)*nw + wi] * weights[wi]
-#              }
-#              tmp <-  survtime[ni] / 2 * survint_i * pre_fac[ni] 
-# 
-#              score_int[ni,] <- tmp * pre_vec[ni, ]
-#              hess_int[ni,] <- tmp * tcrossprod(pre_vec[ni, ])
-#              
-#            }
-#          },
-#          "long" = {
-#            nmarker <- nrow(int_vec)/(nsubj*nw)
-#            if (nmarker %% 1 != 0) {
-#              stop("Dimensions of longitudinal design matrix do not match.")
-#            }
-#            
-#            # Iterate over subjects
-#            for (ni in seq_len(nsubj)) {
-#              
-#              score_i <- 0
-#              hess_i <- 0
-#              
-#              # Iterate over GQ evaluation points
-#              for (wi in seq_along(weights)) {
-#                
-#                tmp <- weights[wi]*omega[(ni-1)*nw + wi]
-#               
-#                score_vec_i <- vector(length = p)
-#                hess_vec_i <- vector(length = p^2)
-#                
-#                # Iterate over markers
-#                for(mi in seq_len(nmarker)) {
-#                  
-#                  score_vec_i <- score_vec_i +
-#                    int_fac[(mi-1)*nsubj*nw + (ni-1)*nw + wi] * 
-#                    int_vec[(mi-1)*nsubj*nw + (ni-1)*nw + wi, ]
-#                  
-#                }
-# 
-#                # Create crossproduct for each row
-#                for (pi in seq_len(p)) {
-#                  for (pi_cross in seq_len(pi)){
-#                    hess_vec_i[(pi-1)*p+pi_cross] <- 
-#                      hess_vec_i[(pi_cross-1)*p+pi] <- 
-#                      score_vec_i[pi] * score_vec_i[pi_cross]
-#                  }
-#                }
-#                
-#                score_i <- score_i + tmp*score_vec_i
-#                hess_i <- hess_i + tmp*hess_vec_i
-#                
-#              }
-#              
-#              # Individual weights
-#              tmp <- survtime[ni] / 2 * pre_fac[ni]
-#              score_int[ni,] <- tmp * score_i
-#              hess_int[ni,] <- tmp * hess_i
-#            }
-#          })
-#   
-#   list(score_int = score_int, hess_int = hess_int)
-# }
+ survint_gq0 <- function (pred = c("lambda", "gamma", "long"), pre_fac,
+                         pre_vec = NULL, omega, int_fac = NULL,
+                         int_vec = NULL, weights, survtime) {
+   
+   # Helpful quantities
+   nw <- length(weights)
+   
+   # Set up the matrices for score and hess
+   nsubj <- length(survtime)
+   p <- if (pred == "gamma") ncol(pre_vec) else ncol(int_vec)
+   score_int <- matrix(0, nrow = nsubj, ncol = p)
+   hess_int <- matrix(0, nrow = nsubj, ncol = p^2)
+   
+   switch(pred, 
+          "lambda" = {
+            
+            # Iterate over subjects
+            for (ni in seq_len(nsubj)) {
+              
+              score_i <- 0
+              hess_i <- 0
+              
+              # Iterate over GQ evaluation points
+              for (wi in seq_along(weights)) {
+                
+                tmp <- weights[wi]*omega[(ni-1)*nw + wi]
+                hess <- vector(length = p^2)
+                # Create crossproduct for each row
+                for (pi in seq_len(p)) {
+                  for (pi_cross in seq_len(pi)){
+                    hess[(pi-1)*p+pi_cross] <- hess[(pi_cross-1)*p+pi] <- 
+                      int_vec[(ni-1)*nw + wi, pi] *
+                      int_vec[(ni-1)*nw + wi, pi_cross]
+                  }
+                }
+                score_i <- score_i + tmp*int_vec[(ni-1)*nw + wi, ]
+                hess_i <- hess_i + tmp*hess
+                
+              }
+              
+              # Individual weights
+              tmp <- survtime[ni] / 2 * pre_fac[ni]
+              score_int[ni,] <- tmp * score_i
+              hess_int[ni,] <- tmp * hess_i
+              
+            }
+          },
+          "gamma" = {
+            
+            # Iterate over individuals
+            for (ni in seq_len(nsubj)) {
+              
+              # Integral is a scalar value
+              survint_i <- 0
+              for (wi in seq_len(nw)) {
+                survint_i <- survint_i +
+                  omega[(ni - 1)*nw + wi] * weights[wi]
+              }
+              tmp <-  survtime[ni] / 2 * survint_i * pre_fac[ni] 
+ 
+              score_int[ni,] <- tmp * pre_vec[ni, ]
+              hess_int[ni,] <- tmp * tcrossprod(pre_vec[ni, ])
+              
+            }
+          },
+          "long" = {
+            nmarker <- nrow(int_vec)/(nsubj*nw)
+            if (nmarker %% 1 != 0) {
+              stop("Dimensions of longitudinal design matrix do not match.")
+            }
+            
+            # Iterate over subjects
+            for (ni in seq_len(nsubj)) {
+              
+              score_i <- 0
+              hess_i <- 0
+              
+              # Iterate over GQ evaluation points
+              for (wi in seq_along(weights)) {
+                
+                tmp <- weights[wi]*omega[(ni-1)*nw + wi]
+               
+                score_vec_i <- vector(length = p)
+                hess_vec_i <- vector(length = p^2)
+                
+                # Iterate over markers
+                for(mi in seq_len(nmarker)) {
+                  
+                  score_vec_i <- score_vec_i +
+                    int_fac[(mi-1)*nsubj*nw + (ni-1)*nw + wi] * 
+                    int_vec[(mi-1)*nsubj*nw + (ni-1)*nw + wi, ]
+                  
+                }
+ 
+                # Create crossproduct for each row
+                for (pi in seq_len(p)) {
+                  for (pi_cross in seq_len(pi)){
+                    hess_vec_i[(pi-1)*p+pi_cross] <- 
+                      hess_vec_i[(pi_cross-1)*p+pi] <- 
+                      score_vec_i[pi] * score_vec_i[pi_cross]
+                  }
+                }
+                
+                score_i <- score_i + tmp*score_vec_i
+                hess_i <- hess_i + tmp*hess_vec_i
+                
+              }
+              
+              # Individual weights
+              tmp <- survtime[ni] / 2 * pre_fac[ni]
+              score_int[ni,] <- tmp * score_i
+              hess_int[ni,] <- tmp * hess_i
+            }
+          })
+   
+   list(score_int = score_int, hess_int = hess_int)
+ }
 
+## New C version.
+survint_C <- function(pred = c("lambda", "gamma", "long"),
+  pre_fac, pre_vec = NULL, omega, int_fac = NULL,
+  int_vec = NULL, weights, survtime)
+{
+  pred <- switch(pred,
+    "lambda" = 1L,
+    "gamma" = 2L,
+    "long" = 3L
+  )
+
+  .Call("survint", pred, pre_fac, pre_vec, omega, int_fac, int_vec, weights, survtime)
+}
+
+if(FALSE) {
+  load(file = "../Fehlersuche/Unterschiede_survint/survint_mjm.RData")
+
+  source("compile.R")
+  source("survint.R")
+  compile()
+
+  int0 <- survint_gq0(pred = "long", pre_fac = exp(eta$gamma),
+    omega = exp(eta_timegrid),
+    int_fac = eta_timegrid_alpha, int_vec = x$Xgrid,
+    weights = gq_weights, survtime = survtime)
+
+  int1 <- survint_C(pred = "lambda", pre_fac = exp(eta$gamma), omega = exp(eta_timegrid), int_fac = eta_timegrid_alpha, int_vec = x$Xgrid, weights = gq_weights, survtime = survtime)
+}
 
 # Old version of survival integral GQ -------------------------------------
-
-
 survint_gq <- function(pred = c("lambda", "gamma", "long"), pre_fac,
                        pre_vec = NULL, omega, int_fac = NULL,
                        int_vec = NULL, weights, survtime) {
