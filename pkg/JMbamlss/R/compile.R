e579b9cb67909eb6a511243dc6489403446f8dc7
alexvolkmann (Alexander Volkmann) <alexander.volkmann@hu-berlin.de>
2022-03-04 08:00:50 +0000

    compile_alex
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@4303 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/JMbamlss/R/compile.R
+++ pkg/JMbamlss/R/compile.R
@@ -22,3 +22,26 @@ compile <- function(dir = NULL, tdir = NULL) {
   setwd(hold)
 }
 
+compile_alex <- function(dir = NULL, tdir = NULL) {
+  hold <- getwd()
+  on.exit(setwd(getwd()))
+  if(is.null(dir)) dir <- "~/Documents/joint_models/bayesr/pkg/JMbamlss/src"
+  dir <- path.expand(dir)
+  if(is.null(tdir)) {
+    dir.create(tdir <- tempfile())
+    on.exit(unlink(tdir), add = TRUE)
+  }
+  tdir <- path.expand(tdir)
+  setwd(tdir)
+  if(file.exists(file.path(dir, "Makevars")))
+    file.copy(file.path(dir, "Makevars"), file.path(tdir, "Makevars"))
+  cf <- grep(".c", dir(dir), value = TRUE, fixed = TRUE)
+  cf <- cf[!grepl(".c~", cf, fixed = TRUE)]
+  cf <- cf[!grepl("init.c", cf, fixed = TRUE)]
+  for(j in cf) {
+    file.copy(file.path(dir, j), file.path(tdir, j))
+    system(paste("R CMD SHLIB", j))
+    dyn.load(gsub(".c", ".so", j, fixed = TRUE))
+  }
+  setwd(hold)
+}
