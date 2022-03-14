42163660518eb376c9c3f68895a2d5534c39d211
alexvolkmann (Alexander Volkmann) <alexander.volkmann@hu-berlin.de>
2022-03-14 20:39:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@4314 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/JMbamlss/R/JM_example.R
+++ pkg/JMbamlss/R/JM_example.R
@@ -34,6 +34,7 @@ if(!exists("d")) {
   
   set.seed(1808)
   d <- simMultiJM(nsub = 50, times = seq(0, 1, length.out = 121),
+                  probmiss = 0.75, maxfac = 1.5, nmark = 2, M = 6, ncovar = 2,
                   lambda = function(t, x) {
                     1.4*log((120*t + 10)/1000)
                   },
@@ -42,10 +43,14 @@ if(!exists("d")) {
                   }), 2),
                   mu = rep(list(function(t, x){
                     1.25 + 0.6*sin(x[, 2]) + (-0.01)*t
-                  }), 2))
+                  }), 2),
+                  mfpc_args = list(type = "split", eFunType = "Poly",
+                                   ignoreDeg = NULL, eValType = "linear",
+                                   eValScale = 1))
   mfpca <- preproc_MFPCA(data = d, uni_mean = "y ~ s(obstime) + s(x2)", M = 2)
   d_simp <- simMultiJM(nsub = 300, times = seq(0, 1, length.out = 121),
-                       nmark = 1, full = TRUE,
+                       probmiss = 0.75, maxfac = 1.5, nmark = 1, M = 6, 
+                       ncovar = 2,
                        lambda = function(t, x) {
                          1.4*log((120*t + 10)/1000)
                        },
@@ -55,7 +60,11 @@ if(!exists("d")) {
                        mu = list(function(t, x){
                          1.25 + 0.6*sin(x[, 2]) + (-0.01)*t
                        }),
-                       sigma = function(t, x) {-50 + 0*t})
+                       sigma = function(t, x) {-50 + 0*t}, 
+                       full = TRUE,
+                       mfpc_args = list(type = "split", eFunType = "Poly",
+                                        ignoreDeg = NULL, eValType = "linear",
+                                        eValScale = 1))
   
 }
 
@@ -64,22 +73,18 @@ if(!exists("d")) {
 
 # PCRE Model --------------------------------------------------------------
 
-# f <- list(
-#   Surv2(survtime, event, obs = y) ~ -1 + s(survtime),
-#   gamma ~ 1,
-#   mu ~ -1 + marker + s(obstime, by = marker) +
-#     s(id, wfpc.1, wfpc.2, bs = "unc_pcre", xt = list("mfpc" = mfpca)),
-#   sigma ~ -1 + marker,
-#   alpha ~ -1 + marker + s(survtime, by = marker)
-# )
-# 
-# b <- bamlss(f, family = mjm_bamlss, data = d, timevar = "obstime",
-#             sampler = FALSE, maxit = 500)
-# load("inst/objects/m_optim.Rdata")
+f <- list(
+  Surv2(survtime, event, obs = y) ~ -1 + s(survtime),
+  gamma ~ 1,
+  mu ~ -1 + marker + s(obstime, by = marker) +
+    s(id, wfpc.1, wfpc.2, bs = "unc_pcre", xt = list("mfpc" = mfpca)),
+  sigma ~ -1 + marker,
+  alpha ~ -1 + marker + s(survtime, by = marker)
+)
 
-# b_sample <- bamlss(f, family = mjm_bamlss, data = d, timevar = "obstime",
-                   # optimizer = FALSE)#, start = parameters(b))
-#, n.iter = 4, burnin = 1, step = 1)
+sink("MJM_pcre.txt")
+b_sample <- bamlss(f, family = mjm_bamlss, data = d, timevar = "obstime")
+sink()
 
 ## Problem bei Alpha-Prädiktor:
 # Warum sollte sich die Likelihood so extrem ändern, nur weil andere alpha-
@@ -92,17 +97,18 @@ if(!exists("d")) {
 
 # RE Model ----------------------------------------------------------------
 
-# f_re <- list(
-#   Surv2(survtime, event, obs = y) ~ -1 + s(survtime),
-#   gamma ~ 1, 
-#   mu ~ -1 + marker + ti(obstime, by = marker) +
-#     ti(id, bs = "re") +
-#     ti(id, obstime, bs = c("re", "cr"), k = c(50, 5)),
-#   sigma ~ -1 + marker,
-#   alpha ~ -1 + marker + s(survtime, by = marker)
-# )
-# b <- bamlss(f_re, family = mjm_bamlss, data = d, timevar = "obstime")
-
+f_re <- list(
+  Surv2(survtime, event, obs = y) ~ -1 + s(survtime),
+  gamma ~ 1,
+  mu ~ -1 + marker + ti(obstime, by = marker) +
+    ti(id, bs = "re") +
+    ti(id, obstime, bs = c("re", "cr"), k = c(50, 5)),
+  sigma ~ -1 + marker,
+  alpha ~ -1 + marker + s(survtime, by = marker)
+)
+sink("MJM_re.txt")
+b <- bamlss(f_re, family = mjm_bamlss, data = d, timevar = "obstime")
+sink()
 
 
 # Very Simple Example ----------------------- ------------------------------
