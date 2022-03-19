59d187feb04b13bdaad241e3301cc2830805ce99
alexvolkmann (Alexander Volkmann) <alexander.volkmann@hu-berlin.de>
2022-03-19 17:22:28 +0000

    Work on comparable fpc simulation (to parametric sim).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@4324 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/JMbamlss/inst/simMJM.R
+++ pkg/JMbamlss/inst/simMJM.R
@@ -121,17 +121,89 @@ compile_alex()
 
 
 sink("sim_f1.txt")
-b_sim <- bamlss(f1, family = mjm_bamlss, data = data_prep, timevar = "obstime",
+b_sim1 <- bamlss(f1, family = mjm_bamlss, data = data_prep, timevar = "obstime",
                 verbose_sampler = TRUE)
 sink()
+save(b_sim1, file = "inst/objects/param_sim01.Rdata")
 
 
 sink("sim_f.txt")
-b_sim <- bamlss(f1, family = mjm_bamlss, data = data_prep, timevar = "obstime",
+b_sim <- bamlss(f, family = mjm_bamlss, data = data_prep, timevar = "obstime",
                 verbose_sampler = TRUE)
 sink()
+save(b_sim, file = "inst/objects/param_sim00.Rdata")
 
 sink("sim_f2.txt")
-b_sim <- bamlss(f2, family = mjm_bamlss, data = data_prep, timevar = "obstime",
+b_sim2 <- bamlss(f2, family = mjm_bamlss, data = data_prep, timevar = "obstime",
                 verbose_sampler = TRUE)
 sink()
+save(b_sim2, file = "inst/objects/param_sim02.Rdata")
+
+
+# Parametric-like PCA Model -----------------------------------------------
+
+dap <- simMultiJM(nsub = 150, times = seq(0, 25, by = 0.25), probmiss = 0.75,
+                  maxfac = 1.5, nmark = 2, param_assoc = FALSE, M = 2, 
+                  FPC_bases = NULL, FPC_evals = NULL,
+                  mfpc_args = list(type = "split", eFunType = "Poly",
+                                   ignoreDeg = NULL, eValType = "linear",
+                                   eValScale = 800), # Vielleicht mal 0.6
+                  ncovar = 2,
+                  lambda = function(t, x) {
+                    1.65 * t^(0.65)
+                  },
+                  gamma = function(x) {
+                    - 5.8 + 0.48*x[, 3]
+                  },
+                  alpha = list(function(t, x) {
+                    0.64 + 0*t
+                  }, function(t, x) {
+                    -0.64 + 0*t
+                  }),
+                  mu = list(function(t, x){
+                    2.13 + 0.24*t - 0.25*x[, 3] - 0.05*t*x[, 3]
+                  }, function(t, x, r){
+                    2.13 + 0.24*t - 0.25*x[, 3] - 0.05*t*x[, 3]
+                  }),
+                  sigma = function(t, x) {
+                    log(0.6) + 0*t
+                  }, 
+                  tmax = NULL, seed = 1808, 
+                  full = TRUE, file = NULL)
+
+plot(dat$fpc_base)
+aha <- unique(dap$data_short[which(dap$data_short$survtime < 25.1 &
+                                     dap$data_short$survtime > 20), "id"])
+
+ggplot(dap$data, aes(x = obstime, y = y, color = id)) +
+  geom_line() +
+  facet_grid(~marker) + 
+  geom_segment(dap$data %>% group_by(id, marker) %>%
+                 filter(obstime == max(obstime),
+                        id %in% aha),
+               mapping = aes(x = obstime, y = y, xend = survtime, yend = y),
+               linetype = "dotted") +
+  geom_point(dap$data %>% group_by(id, marker) %>%
+               filter(obstime == max(obstime),
+                      id %in% aha),
+             mapping = aes(x = survtime, shape = factor(event)))
+
+
+seq <- seq(0, 25, by = 0.25)
+fund <- eFun(argvals = seq, M = 2, type = "Poly")
+fpc_base <- multiFunData(
+  funData(argvals = seq,
+          X = matrix(c(fund@X[1, ],
+                       rep(0, length(seq)),
+                       fund@X[2, ],
+                       rep(0, length(seq))),
+                     nrow = 4, byrow = TRUE)),
+  funData(argvals = seq,
+          X = matrix(c(rep(0, length(seq)),
+                       fund@X[1, ],
+                       rep(0, length(seq)),
+                       fund@X[2, ]),
+                     nrow = 4, byrow = TRUE))
+)
+plot(fpc_base)
+norm(fpc_base)
