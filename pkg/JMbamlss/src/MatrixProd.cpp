498b7e417d42d765d02f157c13ae9acf55a02421
alexvolkmann (Alexander Volkmann) <alexander.volkmann@hu-berlin.de>
2022-06-28 09:28:25 +0000

    C++ code for faster runtime
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesr@4389 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/JMbamlss/src/MatrixProd.cpp
+++ pkg/JMbamlss/src/MatrixProd.cpp
@@ -1,8 +1,10 @@
+// [[Rcpp::depends(RcppEigen)]]
+
 #include <RcppEigen.h>
 
 // [[Rcpp::export]]
 SEXP eigenMapMatMult(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
-    Eigen::MatrixXd C = A * B;
+    Eigen::MatrixXd C = A.adjoint() * B;
 
     return Rcpp::wrap(C);
 }
